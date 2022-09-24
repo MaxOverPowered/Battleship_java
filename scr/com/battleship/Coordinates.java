0b155fa95d79c319d3805b8f94b8a018940e619e
Maxim Copita <copita.maxim.info@gmail.com>
2022-09-24 15:02:49 +0300

    Add Coordinates class with getter

---
+++ scr/com/battleship/Coordinates.java
@@ -0,0 +1,19 @@
+package scr.com.battleship;
+
+public class Coordinates {
+        private final int xCoordinate;
+        private final int yCoordinate;
+
+        public Coordinates(int x, int y) {
+            this.xCoordinate = x;
+            this.yCoordinate = y;
+        }
+
+        public int getX() {
+            return this.xCoordinate;
+        }
+
+        public int getY() {
+            return this.yCoordinate;
+        }
+}
