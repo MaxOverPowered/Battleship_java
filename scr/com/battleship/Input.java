cd1b92b2089f679f9129c3cf99d8f19cbccddc56
Maxim Copita <copita.maxim.info@gmail.com>
2022-09-26 11:42:56 +0300

    Create a validator int input

--- scr/com/battleship/Input.java
+++ scr/com/battleship/Input.java
@@ -11,4 +11,25 @@ public class Input {
         this.scanner = new Scanner(System.in);
         this.display = new Display();
     }
+
+    public int validation() {
+//        Scanner size = new Scanner(System.in);
+        int sizeBord;
+        do {
+//            System.out.println();
+//            System.out.println("Choose a board size between 10 and 20 ");
+            while (!scanner.hasNextInt()) {
+                String userScan = scanner.next();
+                if (!userScan.equalsIgnoreCase("quit")) {
+                    display.printMessageInNewLine("That is not a number,Try again");
+//                    System.out.println("Choose a board size between 10 and 20 ");
+                } else {
+                    System.exit(0);
+                }
+            }
+            sizeBord = scanner.nextInt();
+            display.printMessageInNewLine(String.format("%d",sizeBord));
+            return sizeBord;
+        } while (sizeBord < 5 || sizeBord > 20);
+    }
 }
